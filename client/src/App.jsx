4b2498d2d14a2b50fbb1d3e5076f693393ab9da3
nicole-fabian <102332600+nicolefabian@users.noreply.github.com>
2024-09-16 10:19:57 +1200

    finished the logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,18 @@ import Navbar from "./components/navbar";
 import Home from "./components/home";
 import About from "./components/about";
 import Editor from "./components/editor";
+import LoginPage from "./components/login";
+import PrivateRoute from "./components/privateRoute";
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar /> {/*appear in all components */}
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path="/home" element={<Home />}/>
         <Route path="/about" element={<About />}/>
-        <Route path="/write" element={<Editor />}/>
+        <Route path="/write" element={<PrivateRoute><Editor /></PrivateRoute>}/>
       </Routes>
     </BrowserRouter>
   )
